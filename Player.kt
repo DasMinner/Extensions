d1cfec3c1718d030f5868d2d128eea5dea7dcc00
PHILGAMER <73665072+PHILGAMER@users.noreply.github.com>
2022-01-30 12:49:30 +0100

    Add files via upload

---
+++ Player.kt
@@ -0,0 +1,18 @@
+package de.philgamer.adminfeatures.extensions
+
+import net.axay.kspigot.extensions.geometry.LocationArea
+import org.bukkit.Location
+import org.bukkit.Material
+import org.bukkit.block.Block
+import org.bukkit.entity.Player
+
+fun Player.getUnderMaterial(): Material {
+    return player!!.location.subtract(0.0, 1.0, 0.0).block.type
+}
+
+fun Player.getUnderBlock(): Block {
+    return player!!.location.subtract(0.0, 1.0, 0.0).block
+}
+
+fun Player.isInArea(location: Location, location2: Location): Boolean =
+    LocationArea(location, location2).isInArea(this.location, false, 0)
