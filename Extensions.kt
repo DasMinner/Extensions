abc71d4373c4617d8dd0556ada3ede9162d57cd1
PHILGAMER <73665072+PHILGAMER@users.noreply.github.com>
2022-02-03 18:23:21 +0100

    Delete Extensions.kt

--- Extensions.kt
+++
@@ -1,9 +0,0 @@
-package de.philgamer.uhc.extensions
-
-import org.bukkit.Bukkit
-import org.bukkit.Location
-
-fun location(world: String, x: Int, y: Int, z: Int): Location {
-    if (!Bukkit.getWorlds().contains(Bukkit.getWorld(world))) throw NullPointerException("World $world is null!")
-    return Location(Bukkit.getWorld(world), x.toDouble(), y.toDouble(), z.toDouble())
-}
