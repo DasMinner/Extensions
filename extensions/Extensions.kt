ea7a8ed637c3c1c3a6e6d82d5b6969f589c918d9
PHILGAMER <73665072+PHILGAMER@users.noreply.github.com>
2022-02-04 18:18:16 +0100

    Update Extensions.kt

--- extensions/Extensions.kt
+++ extensions/Extensions.kt
@@ -7,3 +7,20 @@ fun location(world: String, x: Int, y: Int, z: Int): Location {
     if (!Bukkit.getWorlds().contains(Bukkit.getWorld(world))) throw NullPointerException("World $world is null!")
     return Location(Bukkit.getWorld(world), x.toDouble(), y.toDouble(), z.toDouble())
 }
+    fun deleteWorld(world: String) {
+        val worldPath = Bukkit.getWorldContainer().toPath() / world
+        try {
+                Files.walk(worldPath).sorted(Comparator.reverseOrder()).forEach {
+                Files.delete(it)
+            }
+        } catch (e: Exception) {
+            logger.warning("An Error occured while trying to delete the world files ($world)")
+            logger.warning(e.stackTraceToString())
+        }
+        Files.createDirectories(worldPath)
+        Files.createDirectories(worldPath / "data")
+        Files.createDirectories(worldPath / "datapacks")
+        Files.createDirectories(worldPath / "playerdata")
+        Files.createDirectories(worldPath / "poi")
+        Files.createDirectories(worldPath / "region")
+    }
